c67b483f96808e45d4220ebbd5cfb894669b4f45
Knovengel-Nova <>
2026-01-16 11:30:56 +0530

    Initial Files

---
+++ src/main/java/com/project75/app/Project75.java
@@ -0,0 +1,14 @@
+package com.project75.app;
+
+
+
+/**
+ *
+ * @author Aryan
+ */
+public class Project75 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
